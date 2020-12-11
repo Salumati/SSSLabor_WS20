62a0445a339689d107333459de286f46a4d873aa
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2020-12-11 16:25:41 +0100

    added data.csv in git, added beispielcode.py

--- Versuch3/src/aufgabe3.py
+++ Versuch3/src/aufgabe3.py
@@ -13,19 +13,19 @@ data = np.genfromtxt("../media/data.csv", delimiter=",", skip_header=50000)
 plt.title('Floete')
 plt.xlabel('Time')
 plt.ylabel('Voltage(V)')
-plt.yticks(range(-14000, 16000, 2000))
-plt.xticks(range(0, 500, 25))
+# plt.yticks(range(-14000, 16000, 2000))
+# plt.xticks(range(0, 500, 25))
 
-plt.minorticks_on()
+# plt.minorticks_on()
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='black')
-plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+#plt.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
 
 # 15500 gesamt
+'''
 plt.plot(data[25000:25300], "-r")
 plt.savefig('../media/FloeteSignalEinzeln.png', dpi=900)
 plt.show()
-
-
+'''
 ### bestimme anhand des Plots die Grundperiode
 # (etwa 185)(von: 10 bis:195)
 # 0,00419501133786848072562358276644 s pro schwingung
@@ -44,36 +44,43 @@ plt.show()
 M = 225280
 
 ### Abtastintervall deltaT (in s)
-t = 0.00002267573696145127165 # in s
+t = 0.00002267573696145127165  # in s
 #
 
 
 # 3. berechne mit der Funktion numpy.fft.fft()
 # die Fouriertransformierte des Signals und stelle sie graphisch dar
 
-# plot1 = plt.plot(array)
-# plot1.show()
+
 fft = np.fft.fft(data[25000:25500])
+# fft = data[25000:25500]
 plt.title('Fouriertranformierte')
 plt.xlabel('Frequenz(Hz)')
 plt.ylabel('Amplitude')
 plt.grid(True)
 
-print(fft)
-#plt.plot(fft)
-# amplitude = 2 / 500 * np.abs(fft)
-#frequency = np.fft.fftfreq(500)*500*1/()
+xData = [] #Real
+yData = [] #Imag
+
+for n in range(0, len(fft)):
+    yData.append((n / (len(fft) * t)))
+
+x = 500 * t
+xData = np.arange(0, x, t)
+
+
+plt.plot(xData, yData)
+plt.show()
 
 #########################################
-spektrumX = np.zeros(fft.size)
-spektrumY = np.zeros(fft.size)
-
-n = 0
-for value in fft:
-    spektrumX[n] = (n / (t * M))
-    spektrumY[n] = fft[n]
 
 
-plt.savefig('../media/Fouriertranformierte.png', dpi=900)
-plt.plot(spektrumX,spektrumY)
-plt.show()
+# n = 0
+# for value in fft:
+#    spektrumX[n] = (n / (t * M))
+#    spektrumY[n] = fft[n]
+#    n = n + 1
+
+# plt.savefig('../media/Fouriertranformierte.png', dpi=900)
+# plt.plot(spektrumX, spektrumY)
+#plt.show()
